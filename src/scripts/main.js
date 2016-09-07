4d03bae4b771402489f0f90a135c3d107e79b57d
Gregory Smith <crashingwaves@fea.st>
2016-09-07 14:14:04 -0600

    Implement Rx extensions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,27 @@
 import $ from 'jquery';
 import handlebars from 'handlebars';
+import Rx from 'rxjs/Rx';
 
-const existingUserTemplate = handlebars.compile($('#existing-user-template').html()),
-    notExistingUserTemplate = handlebars.compile($('#not-existing-user-template').html());
+const existingUserTemplate = handlebars.compile($('#existing-user-template').html());
 const $userList = $('#user-list');
 
 function getTwitchData(user) {
-    return $.getJSON(`http://localhost:9000/twitch/${user}`);
+    return Rx.Observable.fromPromise($.getJSON(`http://localhost:9000/twitch/${user}`).promise());
 }
 
 $(document).ready(() => {
     if(!localStorage.getItem('firstRun')) {
-        const startingNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+        const startingNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "barflkjasfd"];
         localStorage.setItem('users', JSON.stringify(startingNames));
         localStorage.setItem('firstRun', 'true');
     }
 
-    const userNames = JSON.parse(localStorage.getItem('users'));
-        userNames.forEach(user => {
-            getTwitchData(user).then(data => {
-            if(data.url) {
-                $userList.append(existingUserTemplate(data));
-            } else {
-                $userList.append(notExistingUserTemplate(data));
-            }
-        }).catch(error => {
-            console.error(error);
-        });
+    const userNames = Rx.Observable.from(JSON.parse(localStorage.getItem('users')));
+
+    let rxusernames = userNames.flatMap(getTwitchData);
+    console.log(rxusernames);
+    rxusernames.subscribe(data => {
+        $userList.append(existingUserTemplate(data));
     });
+
 });
