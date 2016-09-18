592e87260d6e6c699de79c0d5e3f7fa25cb645ac
Gregory Smith <crashingwaves@fea.st>
2016-09-18 16:54:46 -0600

    Can add users

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import handlebars from 'handlebars';
 import { Observable, ReplaySubject } from 'rxjs/Rx';
+import toastr from 'toastr';
 window.$ = window.jQuery = $;
 require('bootstrap/js/modal');
 $.noConflict(true);
@@ -13,7 +14,8 @@ const existingUserTemplate = handlebars.compile($('#existing-user-template').htm
         $buttonInput = $('.sort'),
         $dataList = $('#users'),
         $searchInput = $('#search-input'),
-        gettingFilterInput = Observable.fromEvent($('#filter-input'), 'keyup')
+        $filterInput = $('#filter-input'),
+        gettingFilterInput = Observable.fromEvent($filterInput, 'keyup')
                         .map(e => e.target.value)
                         .startWith(''),
         gettingButtonInput = Observable.fromEvent($buttonInput, 'click')
@@ -24,16 +26,14 @@ const existingUserTemplate = handlebars.compile($('#existing-user-template').htm
                         .map(e => $(e.target).data('value'))
                         .startWith('all'),
         gettingSearchInput = Observable.fromEvent($searchInput, 'keyup')
-                        .do(e => {
-                            if(e.target.value.length === 0) {
+                        .do(event => {
+                            if(event.target.value.length === 0) {
                                 $dataList.html('');
-                            } else {
-                                $dataList.show();
                             }
                         })
                         .debounceTime(500)
-                        .filter(e => e.which >= 48 && e.which <= 90)
-                        .filter(e => e.target.value.length > 2)
+                        .filter(event => (event.which === 8 || event.which >= 48 && event.which <= 90))
+                        .filter(event => event.target.value.length > 2)
                         .distinctUntilChanged()
                         .map(e => e.target.value),
         storedUsers = new ReplaySubject();
@@ -48,14 +48,16 @@ Observable.fromEvent(document, 'DOMContentLoaded')
     });
 
 Observable.fromEvent($('.add'), 'click')
-    .do(e => {
-        e.preventDefault();
+    .do(event => {
+        event.preventDefault();
     })
     .subscribe(e => {
         if($searchInput.hasClass('activated')) {
             $searchInput.removeClass('activated');
+            $filterInput.focus();
         } else {
             $searchInput.addClass('activated');
+            $searchInput.focus();
         }
     })
 
@@ -66,13 +68,19 @@ Observable.fromEvent($userModal, 'hidden.bs.modal')
 
 Observable.fromEvent($dataList, 'click')
     .map(event => $(event.target).closest('.result-name').html())
+    .filter(name => name !== "undefined" && name !== null && name.length > 0)
     .do(user => {
-        e.stopPropagation();
         let currentUsers = JSON.parse(localStorage.getItem('users'));
-        currentUsers.push(user);
-        localStorage.setItem('users', JSON.stringify(currentUsers));
-
-        $dataList.hide();
+        if(currentUsers.indexOf(user) === -1) {
+            console.log(currentUsers.indexOf(user));
+            currentUsers.push(user);
+            localStorage.setItem('users', JSON.stringify(currentUsers));
+            toastr.success("User added!");
+            $dataList.hide();
+            $searchInput.val('');
+        } else {
+            toastr.warning("User already on list");
+        }
     })
     .flatMap(user => Observable.ajax({
         url:`http://localhost:9000/twitch/${user}`,
@@ -88,11 +96,15 @@ Observable.fromEvent($searchInput, 'focus')
     });
 
 gettingSearchInput
+    .do(e => {
+        $dataList.html('');
+    })
     .flatMap(search => Observable.ajax(`http://localhost:9000/twitch/search/${search}`))
     .map(response => response.response)
     .flatMap(user => user)
     .subscribe(result => {
         $dataList.append(userResultTemplate(result));
+        $dataList.show();
     })
 
 const getUsers = Observable.create(observer => {
@@ -127,5 +139,31 @@ gettingFilterInput
         $userList.append(existingUserTemplate(data))
     });
 
+Observable.fromEvent($userModal, 'click')
+    .filter(event => $(event.target).hasClass('delete-button'))
+    .map(event => $(event.target).data('name'))
+    .subscribe(name => {
+        const currentUsers = JSON.parse(localStorage.getItem('users')),
+                idx = currentUsers.findOne(name);
+                //TODO Error handling
+                $userModal.modal('hide');
+                toastr.error('User deleted');
+                currentUsers.splice(idx, 1);
+                localStorage.setItem('users', JSON.stringify(currentUsers));
+                // $userList.html('');
+                const userToGo = $userList.find(`#${name}`);
+                console.log(userToGo);
+                userToGo.remove();
+    });
 
 handlebars.registerHelper('jsonify', object => JSON.stringify(object));
+
+Array.prototype.findOne = function (search){
+    let idx;
+    for(let i = 0; i < this.length; i++) {
+        if(this[i].toLowerCase() === search.toLowerCase()) {
+            return i;
+        }
+    }
+    return -1;
+}
